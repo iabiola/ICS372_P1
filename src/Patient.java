556955e6d95d251b06a4ebe33940251120166bba
iabio <iabio@10.15.20.41>
2018-01-27 14:33:37 -0600

    Patient class

---
+++ src/Patient.java
@@ -0,0 +1,37 @@
+
+
+public class Patient {
+	
+	public String patient_id;
+    public boolean patient_active;
+	
+    public Patient(String patient_id, boolean patient_active) {
+		
+		this.patient_id = patient_id;
+		this.patient_active = patient_active;
+	}
+
+	public String getPatient_id() {
+		return patient_id;
+	}
+
+	public void setPatient_id(String patient_id) {
+		this.patient_id = patient_id;
+	}
+
+	public boolean isPatient_active() {
+		return patient_active;
+	}
+
+	public void setPatient_active(boolean patient_active) {
+		this.patient_active = patient_active;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [patient_id=" + patient_id + ", patient_active=" + patient_active + "]";
+	}
+    
+    
+
+}
