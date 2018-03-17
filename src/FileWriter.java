bc81687e46e0aeb024db5502ac2911031d95fc0e
dave_pierce <dave_pierce@dell.com>
2018-03-17 14:45:34 -0500

    IO Classes

---
+++ src/FileWriter.java
@@ -0,0 +1,19 @@
+
+public class FileWriter {
+
+	private FileWriter() {
+		return;
+		// TODO Auto-generated constructor stub
+	}
+
+	public void write(Object e)
+	{
+		
+	}
+	
+	public void serialize(Object e)
+	{
+		
+	}
+	
+}
