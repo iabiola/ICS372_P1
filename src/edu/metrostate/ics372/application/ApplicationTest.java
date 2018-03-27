09dc42f304262e8c9502d21e84ebad1207bf4e28
sckang511 <sckang511@sckang511-XPS-13-9360>
2018-03-27 00:16:21 -0500

    latest updates

---
+++ src/edu/metrostate/ics372/application/ApplicationTest.java
@@ -0,0 +1,14 @@
+package edu.metrostate.ics372.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/edu/metrostate/ics372/ui/ControllerTest.java
@@ -0,0 +1,14 @@
+package edu.metrostate.ics372.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
