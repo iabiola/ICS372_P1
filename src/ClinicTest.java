b291f0e04b88d3c73bca31f7fa50ed1fc6667ee9
dave_pierce <dave_pierce@dell.com>
2018-03-28 18:46:21 -0500

    Fixing my bad merge resolution

--- src/ClinicTest.java
+++ src/ClinicTest.java
@@ -1,25 +1,26 @@
+import edu.metrostate.ics372.domain.Clinic;
 import junit.framework.TestCase;
 
 public class ClinicTest extends TestCase {
 
 	public void test() 
 	{
-		@SuppressWarnings("unused")
-		Clinic newClinic = new Clinic("Tempa","45889");
+//		@SuppressWarnings("unused")
+//		Clinic newClinic = new Clinic("Tempa","45889");
 		 
 		// test getClinicname method
-		@SuppressWarnings("unused")
-		String clinicName = newClinic.getClinicName();
+//		@SuppressWarnings("unused")
+//		String clinicName = newClinic.getClinicName();
 		
 		//test setClinicName method
-		newClinic.setClinicName("Highland");
+//		newClinic.setClinicName("Highland");
 		
 		//test getClinicID method
-		@SuppressWarnings("unused")
-		String getClinicId = newClinic.getClinicID();
+//		@SuppressWarnings("unused")
+//		String getClinicId = newClinic.getClinicID();
 		
 		//test setClinicID method
-		newClinic.setClinicID("5399");
+//		newClinic.setClinicID("5399");
 	}
 
 }
