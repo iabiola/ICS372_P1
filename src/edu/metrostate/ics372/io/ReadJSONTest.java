0659dfb65e93e9a11b1fdf98757902ae93e0d64c
dave_pierce <dave_pierce@dell.com>
2018-03-28 20:33:41 -0500

    Move tests to correct directory

--- src/edu/metrostate/ics372/io/ReadJSONTest.java
+++ src/edu/metrostate/ics372/io/ReadJSONTest.java
@@ -1,14 +1,33 @@
-package edu.metrostate.ics372.io;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-import org.junit.jupiter.api.Test;
+import org.json.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
-class ReadJSONTest {
+import junit.framework.TestCase;
 
+public class ReadJSONTest extends TestCase{
+
+	// test read method of type jArray.
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void test() throws JsonIOException, JsonSyntaxException, FileNotFoundException 
+	{
+		JSONArray obj2 = new JSONArray();
+		JsonParser parse = new JsonParser();
+		JsonElement jTree = parse.parse("filePath");
+        obj2.put("record_id");
+        obj2.put("patient");
+		obj2.put(parse);
+		obj2.put(jTree);
+		Assert.assertFalse(jTree.isJsonNull());
+    
 	}
-
+  
 }
